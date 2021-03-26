42c966b9a911b0e9e4563cf3751fa7baab2959e6
Bacon Fixation <baconfixation@gmail.com>
2021-03-26 09:41:58 -0500

    Update bot-info.js
    memberName 'bot-info' is used by the about.js command
    Optimized 
    Auto > No more hard coded info
    Removed all Vars > Data is constant
    Fixed map 
    Formatted to match Current Master-Bot Formatting
    
    Much Love
    -Bacon

--- bot-info.js
+++ bot-info.js
@@ -1,54 +1,52 @@
-var commando = require('discord.js-commando');
-var discord = require('discord.js');
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
 
-class aboutuser extends commando.Command 
-{
-    constructor(client) {
-        super(client, {
-            name: 'bot-info',
-            group: 'guild',
-            memberName: 'abot-info',
-            description: 'Lists information about a specific user.',
-            aliases: ['bi', 'boti', 'binfo', 'bot'],
-        })
-    }
-async run(message, args){
+module.exports = class AboutUserCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'bots-info',
+      memberName: 'bots-info',
+      group: 'other',
+      description: 'Lists information about the bot.',
+      aliases: ['bi', 'boti', 'binfo', 'bot']
+    });
+  }
 
-    let userinfo = {};
-    userinfo.bot = message.client.user.bot;
-    userinfo.createdat = message.client.user.createdAt;
-    userinfo.discrim = message.client.user.discriminator;
-    userinfo.id = message.client.user.id;
-    userinfo.mfa = message.client.user.mfaEnabled;
-    userinfo.pre = message.client.user.premium;
-    userinfo.presen = message.client.user.presence;
-    userinfo.tag = message.client.user.tag;
-    userinfo.uname = message.client.user.username;
-    userinfo.verified = message.client.user.verified;
+  run(message) {
+    const userInfo = {
+      bot: message.client.user.bot,
+      createAt: message.client.user.createdAt,
+      discriminator: message.client.user.discriminator,
+      id: message.client.user.id,
+      mfa: message.client.user.mfaEnabled,
+      premium: message.client.user.premium,
+      presence: message.client.user.presence,
+      tag: message.client.user.tag,
+      userName: message.client.user.username,
+      verified: message.client.user.verified,
+      avatar: message.client.user.avatarURL
+    };
 
-    userinfo.avatar = message.client.user.avatarURL;
+    const myInfo = new MessageEmbed()
+      .setTitle(`INFORMATION ABOUT '${this.client.user.username}'`)
+      .setAuthor(userInfo.userName, this.client.user.displayAvatarURL())
+      .setThumbnail(this.client.user.displayAvatarURL())
+      .setColor('#F0E5DE')
+      .addField('Bot?', userInfo.bot, true)
+      .addField('Created At', `${userInfo.createAt.toLocaleString()}`, true)
+      .addField('Discriminator', userInfo.discriminator, true)
+      .addField('Client ID', userInfo.id, true)
+      .addField('2FA/MFA Enabled?', userInfo.mfa, true)
+      .addField('Presence', userInfo.presence, true)
+      .addField('Client Tag', userInfo.tag, true)
+      .addField('Username', userInfo.userName, true)
+      .addField('Verified?', userInfo.verified, true)
+      .setFooter(
+        `Created By -> '${this.client.owners[0].username}' since`,
+        this.client.user.avatarURL()
+      )
+      .setTimestamp(userInfo.createdAt);
 
-    var myInfo = new discord.MessageEmbed()
-        .setAuthor(userinfo.uname, "https://cdn.discordapp.com/avatars/822484015801040896/615a3ef2637eba811eccf2995106f33a.png")
-        .addField("`Bot?`",userinfo.bot, true)
-        .addField("`Created At`","Fri Mar 19 2021", true)
-        .addField("`Discriminator`",userinfo.discrim, true)
-        .addField("`Client ID`",userinfo.id, true)
-        .addField("`2FA/MFA Enabled?`",userinfo.mfa, true)
-        .addField("`Presence`",userinfo.presen, true)
-        .addField("`Client Tag`",userinfo.tag, true)
-        .addField("`Username`",userinfo.uname, true)
-        .addField("`Verified?`",userinfo.verified, true)
-        .setColor(0xf0e5da)
-        .setFooter('Created By -> IG🔰VOLT™ | ΜΔĐ乛TeamIG™#8929 since', "https://cdn.discordapp.com/avatars/689388891106705467/a_fdad411f1a344a374ac6dbd8089607be.gif")
-        .setTitle("INFORMATION ABOUT `WoW Music`")
-        .setThumbnail("https://cdn.discordapp.com/avatars/822484015801040896/615a3ef2637eba811eccf2995106f33a.png")
-        .setTimestamp(userinfo.createdat)
-
-
-        message.channel.send(myInfo);
-
-}
-
-}
-module.exports = aboutuser;
+    message.channel.send(myInfo);
+  }
+};
